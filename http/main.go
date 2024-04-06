af1520cbbb52c3946c901acac1a651d8e9a84fa3
aleksandras.artemjevas@mambu.com <aleksandras.artemjevas@mambu.com>
2024-04-06 16:10:06 +0300

    http update

--- http/main.go
+++ http/main.go
@@ -1,29 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 )
 
-type logWriter struct {
-}
-
-func (logWriter) Write(bs []byte) (int, error) {
-	fmt.Println(string(bs))
-	return len(bs), nil
-
-}
-
 func main() {
 	resp, err := http.Get("https://jsonplaceholder.typicode.com/users/1")
 	if err != nil {
-		fmt.Println("Error:", err)
+		log.Fatalln(err)
 		os.Exit(1)
 	}
 
-	lw := logWriter{}
-	io.Copy(lw, resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// sb := string(body)
+	// log.Println(sb)
+
+	var result map[string]any
+	json.Unmarshal(body, &result)
+
+	address := result["address"].(map[string]any)
+	fmt.Println(address["city"])
 
 }
